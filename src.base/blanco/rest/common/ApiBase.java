21d65a5e7c51add2ff0157f8d27c6061e149c9cb
tueda <ueo@ueo.co.jp>
2015-10-08 17:26:48 +0900

    Add jackson core jar. rename directory name of non-auto generated sources.

--- src.base/blanco/rest/common/ApiBase.java
+++
@@ -1,7 +0,0 @@
-package blanco.rest.common;
-
-/**
- * Created by tueda on 15/10/05.
- */
-public class ApiBase {
-}
---
+++ src.main/blanco/rest/common/ApiBase.java
@@ -0,0 +1,15 @@
+package blanco.rest.common;
+
+import blanco.sample.valueobject.ApiTelegram;
+
+/**
+ * Created by tueda on 15/10/05.
+ */
+public class ApiBase {
+
+    final protected ApiTelegram send(ApiTelegram request) {
+        ApiTelegram response = null;
+
+        return response;
+    }
+}
